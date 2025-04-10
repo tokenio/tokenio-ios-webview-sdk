21d63aa14bf3865099114f09fc9406639e7f71e7
Josh Lister <joshlister20@gmail.com>
2025-04-10 09:50:19 +0100

    Initial Commit

---
+++ TokenTestiOS/TokenTestiOSApp.swift
@@ -0,0 +1,32 @@
+//
+//  TokenTestiOSApp.swift
+//  TokenTestiOS
+//
+//  Created by Josh Lister on 10/04/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TokenTestiOSApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
